e892bc4aa94bb3018ca4c8ecf275339e2b6dc708
Ishani <ishaniamin@gmail.com>
2018-06-18 16:02:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,10 @@ class Song
     self.class.all << self
   end
 
-  def create
+  def self.create
     song = Song.new
-    @all << song
-    song
+    @@all << song
+    song 
   end
 
   def self.new_by_name(name)
